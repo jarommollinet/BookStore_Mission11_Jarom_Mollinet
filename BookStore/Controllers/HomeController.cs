26a51aeea04e1821af95ce2708f8d5b7e173bd64
jarommollinet <jarommollinet@gmail.com>
2024-03-22 14:38:30 -0600

    Final Mission 11

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using BookStore.Models;
+using BookStore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -13,11 +14,28 @@ namespace BookStore.Controllers
             _repo = temp;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageNum)
         {
-           var bookData = _repo.Books;
 
-            return View(bookData);
+            int pageSize = 10;
+
+            var blah = new BookListViewModel
+            {
+                Books = _repo.Books
+                 .OrderBy(x => x.Title)
+                 .Skip((pageNum - 1) * pageSize)
+                 .Take(pageSize),
+
+                PaginationInfo = new PaginationInfo
+                {
+                    CurrentPage = pageNum,
+                    ItemsPerPage = pageSize,
+                    TotalItems = _repo.Books.Count()
+                }
+            };
+                 
+
+            return View(blah);
         }
     }
 }
